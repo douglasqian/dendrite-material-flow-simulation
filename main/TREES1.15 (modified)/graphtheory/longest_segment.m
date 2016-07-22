e48c54aa2848e8c6e83fffeff4131646e0531677
Douglas Qian <rubbertinkerbell@gmail.com>
2016-07-22 12:42:01 -0700

    kymograph, heat map and additional features

--- main/TREES1.15 (modified)/graphtheory/longest_segment.m
+++ main/TREES1.15 (modified)/graphtheory/longest_segment.m
@@ -1,10 +1,10 @@
-function [minlen, minid] = shortest_segment(intree)
+function [maxlen, maxid] = longest_segment(intree)
     inv_dA = intree.dA';
     [row, col] = find(inv_dA);
     sects = [row, col]; % all segments between points
     plen = Pvec_tree(intree); % cumulative path lengths to root
     plen(sects);
     slen = diff(plen(sects), [], 2); % all segment lengths
-    minlen = min(slen);
-    minid = sects(find(slen == minlen), 2);
+    maxlen = max(slen);
+    maxid = sects(find(slen == maxlen), 2);
 end
--- main/matflowsim.m
+++ main/matflowsim.m
@@ -1,4 +1,4 @@
-% MATFLOWSIM -- simulates unidrectional cargo (particle) transport in neuron dendrites
+% MATFLOWSIM (v2.3) - dendritic bidrectional cargo transport simulator
 % 
 % matflowsim (path, varargin)
 % -----------------------------------
@@ -13,25 +13,28 @@
 %         
 % Optional Parameters
 %
-%     - rule::    string: rule determining probabilities for cargo choosing 
-%                 children neurites at branch points (must be a TREES toolbox 
-%                 function that returns a column vector) {DEFAULT: 'cvol_tree'}
+%     - rule::    string: name of branching rule {DEFAULT: 'cvol_tree'}
 %
-%     - rtime::   scalar: total run time of simulation {DEFAULT: 60}
+%     - rtime::   scalar: total run time of simulation movie [s] {DEFAULT: 30}
 % 
-%     - freq::    scalar: frequency of new cargo generation (sec) {DEFAULT: 5}
+%     - freq::    scalar: frequency of new cargo generation [s] {DEFAULT: 2}
 % 
-%     - amp::     scalar: number of cargo generated each wave {DEFAULT: 3}
+%     - amp::     scalar: number of cargo generated in each wave {DEFAULT: 5}
 % 
-%     - mvel::    scalar: mean velocity of cargo {DEFAULT: 1}
+%     - mvel::    scalar: mean velocity of cargo [micron/s] {DEFAULT: 1}
 %
-%     - sprob::   scalar: probability for cargo's direction to change {DEFAULT: 0.005}
+%     - radius::  scalar: radius of cargo {DEFAULT: 4}
 %
-%     - xspeed::  scalar: simulation speed-up factor {DEFAULT: 1}
+%     - sprob::   scalar: probability for cargo's direction to change {DEFAULT: 0.01}
 %
-%     - options:: string: using special simulation features {DEFAULT: '-m'}
+%     - xspeed::  scalar: simulation speed-up factor {DEFAULT: 8}
+%
+%     - options:: string: using special simulation features {DEFAULT: '-s'}
+%                 '-s': resample neuron tree for faster simulation
 %                 '-m': make .mp4 movie out of frame data
-%                 '-s': simplify neuron tree morphology for fast simulation
+%                 '-h': ptle density heat map
+%                 '-k': kymograph
+%                 '-d': display statistics
 %
 % Example
 % -------
@@ -41,14 +44,18 @@
 
 function matflowsim(path, varargin)
     %% PARSING INPUT ARGUMENTS %%
+    
     p = inputParser;
-    % Setting validation functions
+    
+    % Input validation functions
     valfcn1 = @(x) validateattributes(x, {'char'},   {'nonempty'});
     valfcn2 = @(x) validateattributes(x, {'double'}, {'scalar', 'positive', 'nonempty'});
+    
     % Parameter names, default values, and corresponding validation functions
-    params = {'path'  'rule'       'rtime'  'freq'  'amp'  'mvel'  'sprob'  'xspeed'  'options'};
-    dfvals = {NaN     'cvol_tree'   60       5       3      1       0.005    1        '-m'     };
-    valfcn = [1        1            2        2       2      2       2        2         1       ];
+    params = {'path'  'rule'       'rtime'  'freq'  'amp'  'mvel'  'radius'  'sprob'  'xspeed'  'options'};
+    dfvals = {NaN     'cvol_tree'   30       2       5      1       4         0.00     8        '-s'     };
+    valfcn = [1        1            2        2       2      2       2         2        2         1       ];
+    
     % Adding arguments to input parser scheme
     for i = 1: length(params)
         if valfcn(i) == 1
@@ -61,8 +68,10 @@ function matflowsim(path, varargin)
             addParameter(p, params{i}, dfvals{i}, valfcn2);
         end
     end
-    % Parsing function inputs
+    
+    % Parsing inputs
     parse(p, path, varargin{:});
+    
     % Extracting variable values
     path    = p.Results.path;
     rule    = str2func(p.Results.rule);
@@ -70,34 +79,94 @@ function matflowsim(path, varargin)
     freq    = p.Results.freq;
     amp     = p.Results.amp;
     mvel    = p.Results.mvel;
+    radius  = p.Results.radius;
     sprob   = p.Results.sprob;
     xspeed  = p.Results.xspeed;
     options = p.Results.options;
-    % fprintf('path: %s\nrule: %s\nrtime: %d\nfreq: %d\namp: %d\nmvel: %f\nsprob: %f\noptions: %s\n', path, rule, rtime, freq, amp, mvel, sprob, options)
     
     %% SIMULATION SETUP %%
+    
+    % Extracting neuron morphology from file
     sneuron = load_tree(path);
+    
+    % Probabilities generated based off of rule (Nx1 vector)
     rule_data = asym_tree(sneuron, rule(sneuron));
-    if ~isempty(options) && ~isempty(strfind(options, '-s'))
-        sneuron = simplify_tree(sneuron);
+    
+    % Simplifying (using TREEs resample_tree) the morphology
+    if ~isempty(strfind(options, '-s'))
+        sneuron = resample_tree(sneuron, 10);
         rule_data = asym_tree(sneuron, rule(sneuron));
     end
-    % 2D plotting of neuron morphology
-    f = figure;
-    set(f, 'Position', [0 0 1000 1000], 'Color', 'white')
-    plot_tree(sneuron, [0 0 0], [0 0 0], [], 8, '-2q');
-    axis off, hold on,
-    % Cargo (ptle) parameters
-    radius = 4;
-    step_size = shortest_segment(sneuron);
-    ptles = struct([]);
-    % mean velocity (~1 micron/s) standardization
-    framerate = mvel/step_size*xspeed;
     
-    typetree = typeN_tree(sneuron);
+    % Neuron morphology plotting
+    f1 = figure;
+    f1.Name = 'Simulation'; f1.Position = [0 0 1000 1000]; f1.Color = 'white';
+    plot_tree(sneuron);
+    axis off, hold on,
+    
+    % Calculating step size 
+    step_size = shortest_segment(sneuron);
+    
+    % Setting up kymograph parameters
+    kymo = 0;
+    if ~isempty(strfind(options, '-k'))
+        % Kymograph region of interest is longest segment
+        [~,kroi] = longest_segment(sneuron);
+        % Kymograph only samples middle-third of simulation
+        kdur = [round(0.33*rtime) round(0.67*rtime)];
+        kymo = 1;
+    end
+    
+    % Options switches
+    dstats = 0;
+    if ~isempty(strfind(options, '-d'))
+        dstats = 1;
+    end
+    
+    hmap = 0;
+    if ~isempty(strfind(options, '-h'))
+        hmap = 1;
+    end
+    
+    % Unique cargo ID's
+    ptleidct = 0;
+    ptids    = transpose(1:length(sneuron.dA)); % Nx1 vector storing id's of points
+    
+    % Pre-existing cargo
+    branchpts = [sneuron.X(B_tree(sneuron)) sneuron.Y(B_tree(sneuron)) ptids(B_tree(sneuron))];
+    branchpts(1,:) = [];
+    ptles(length(branchpts)).target = NaN; % struct array storing information about all cargo particles
+    for pt = 1: length(branchpts)
+       if rand >= 0.5
+           ptles(pt).graphics = plot(branchpts(pt,1), branchpts(pt,2), 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'green', 'MarkerSize', radius);
+           ptles(pt).dir = 1;
+       else
+           ptles(pt).graphics = plot(branchpts(pt,1), branchpts(pt,2), 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'red', 'MarkerSize', radius);
+           ptles(pt).dir = 0;
+       end
+       ptles(pt).target = [find(sneuron.dA(branchpts(pt,3),:)) branchpts(pt,3)];
+       ptles(pt).speed  = normrnd(step_size, step_size*0.25, 1);
+       ptles(pt).id     = ptleidct;
+       ptleidct         = ptleidct + 1;
+    end
+    
+    % Other data structures
+    if hmap
+        ptcoords  = []; % Zx2 vector of ptle coordinates
+    end
+    if dstats
+        density   = zeros(length(sneuron.dA), 1); % Nx1 vector of ptle counts
+    end
+    typetree  = typeN_tree(sneuron); % Nx1 vector of point types
+    endpts    = [sneuron.X(T_tree(sneuron)) sneuron.Y(T_tree(sneuron)) ptids(T_tree(sneuron))]; % Mx2 vector of endpoint coordinates
     
     %% MOVIE MAKER %%
-    if ~isempty(options) && ~isempty(strfind(options, '-m'))
+    
+    % Simulation speed standardization
+    framerate = round(mvel/step_size*xspeed);
+    
+    % Creating movie
+    if ~isempty(strfind(options, '-m'))
         if ~isempty(options) && ~isempty(strfind(options, '-s'))
             vname = sprintf('%s simplified (%s %0.2fmps)', sneuron.name(1:end-4), func2str(rule), mvel*xspeed);
         else
@@ -109,45 +178,102 @@ function matflowsim(path, varargin)
     end
 
     %% ZOOM HANDLING %%
-    orig_ax = axis;
-    h = zoom;
-    set(h,'ActionPostCallback',@zoomCallBack);
-    function zoomCallBack(~, evd)      
+    z = zoom;
+    set(z,'ActionPreCallBack', @zoomPreCallBack, 'ActionPostCallback', @zoomPostCallBack);
+    last_ax = axis;
+    function zoomPreCallBack(~, evd)
+       last_ax = axis(evd.Axes); 
+    end
+    function zoomPostCallBack(~, evd)      
         new_ax = axis(evd.Axes);    
         % Zoom based on changes in y-axis lengths
-        orig_len = orig_ax(4)-orig_ax(3);
+        last_len = last_ax(4)-last_ax(3);
         new_len = new_ax(4)-new_ax(3);
+        radius = radius * (last_len/new_len);
         for pt = 1: length(ptles)
-            set(ptles(pt).graphics, 'MarkerSize', radius*(orig_len/new_len))
+            set(ptles(pt).graphics, 'MarkerSize', radius)
         end
     end
 
     %% SIMULATION LOOP %%
-    iterations = ceil(rtime * framerate);
     
+    % NOTE: 1 iteration = 1 frame
+    iterations = rtime * framerate;
+
     for i = 0: iterations
        fprintf('%d\n', i)
-       % Generating new ptles
-       if mod(i, round(freq*framerate)) == 0
-           for j = 1: amp
-               ptles(end+1).graphics = plot(sneuron.X(1), sneuron.Y(1), 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'green', 'MarkerSize', radius);
-               ptles(end).target = [NaN 1];
-               ptles(end).dir = 1;
+       if mod(i, freq*framerate) == 0
+           % Generate cargo coming from soma (root) and endpts
+           tpoints = randperm(size(endpts, 1), amp);
+           for j = 1: 2*amp
+               if j <= amp
+                   % Coming from endpts (endocytosis) --> red, dir = 0
+                   tid = endpts(tpoints(j), 3);
+                   ptles(end+1).graphics = plot(endpts(tpoints(j), 1), endpts(tpoints(j), 2), 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'red', 'MarkerSize', radius);
+                   ptles(end).target = [find(sneuron.dA(tid,:)) tid];
+                   ptles(end).dir = 0;
+               else
+                   % Going towards endpts (exocytosis) --> green, dir = 1
+                   ptles(end+1).graphics = plot(sneuron.X(1), sneuron.Y(1), 'o', 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'green', 'MarkerSize', radius);
+                   ptles(end).target = [NaN 1];
+                   ptles(end).dir = 1;
+               end
                ptles(end).speed = normrnd(step_size, step_size*0.25, 1);
+               ptles(end).id = ptleidct;
+               ptleidct = ptleidct + 1;
            end
        end
-       % Move all ptles
+       
+       % Start logging data for kymograph
+       if kymo && i == kdur(1) * framerate
+           kids = []; % tracking ptle id's
+           kdata = zeros(diff(kdur)*framerate, 1); % distance of ptles from start of kroi
+           % NOTE: length(kids) should equal size(kdata, 3) 
+           
+       % Stop logging data for kymograph    
+       elseif kymo && i == kdur(2)* framerate
+           kymo = 0;
+       end
+       
+       % LOOPING THROUGH ALL PTLES %
        for pt = 1: length(ptles)
-           x1 = ptles(pt).graphics.XData;
-           y1 = ptles(pt).graphics.YData;
-           dir = ptles(pt).dir;
+           % Accounts for ptles deleted within one iteration
+           if pt > length(ptles)
+               break
+           end
+           
+           % Cargo position data
+           x1     = ptles(pt).graphics.XData;
+           y1     = ptles(pt).graphics.YData;
+           dir    = ptles(pt).dir;
            target = ptles(pt).target(dir+1);
-           x2 = sneuron.X(target);
-           y2 = sneuron.Y(target);
-%            fprintf('-----------\n')
-%            fprintf('X: %f Y: %f\n', x1, y1)
-%            fprintf('DIR: %d target: %d\n',dir, target)
+           x2     = sneuron.X(target);
+           y2     = sneuron.Y(target);
+           
+           % Logging kymograph data
+           if kymo && i >= kdur(1) * framerate && ptles(pt).target(2) == kroi
+               num = find(kids == ptles(pt).id);
+               ptarget = ptles(pt).target(1); % or find(sneuron.dA(kroi,:))
+               px = sneuron.X(ptarget); py = sneuron.Y(ptarget);
+               dist = pdist([x1 y1; px py], 'euclidean');
+               if ~isempty(num)
+                   kdata(i-kdur(1)*framerate+1, 1, num) = dist;
+               else
+                   kids(end+1) = ptles(pt).id;
+                   nnum = find(kids == ptles(pt).id);
+                   kdata(i-kdur(1)*framerate+1, 1, nnum) = dist;
+               end
+           end
+           
+           % Logging heat map and statistics data
+           if dstats
+               density(ptles(pt).target(2)) = density(ptles(pt).target(2)) + 1;
+           end
+           if hmap
+               ptcoords = [ptcoords; x1 y1];
+           end
                
+           % Checking if cargo is close enough to target
            if pdist([x1 y1; x2 y2], 'euclidean') < step_size
                % Jump to point first
                ptles(pt).graphics.XData = x2;
@@ -175,14 +301,14 @@ function matflowsim(path, varargin)
                    else
                        delete(ptles(pt).graphics);
                        ptles(pt) = [];
-                       break
+                       continue
                    end
                % BACKWARD MOVING
                else
                    if target == 1
                        delete(ptles(pt).graphics);
                        ptles(pt) = [];
-                       break
+                       continue
                    end
                    ptles(pt).target = [find(sneuron.dA(target, :)) target];
                end
@@ -211,18 +337,56 @@ function matflowsim(path, varargin)
            if change < sprob
                ptles(pt).dir = ~ptles(pt).dir;
            end
-           
        end
               
        drawnow
        
-       if ~isempty(options) && ~isempty(strfind('-m', options))
+       % Write frame into movie object
+       if ~isempty(strfind(options, '-m'))
            writeVideo(v, getframe(gcf));
        end
        
     end
     
-    if ~isempty(options) && ~isempty(strfind('-m', options))
+    %% HEAT MAP %%
+    if hmap
+        f2 = figure;
+        f2.Name = 'Heat Map';
+        [values, centers] = hist3(ptcoords, [200,200]);
+        imagesc(centers{:}, values);
+        colormap jet,
+        colorbar,
+    end
+    
+    %% KYMOGRAPH %%
+    if ~isempty(strfind(options, '-k'))
+        f3 = figure;
+        f3.Name = 'Kymograph';
+        timescale = kdur(1): 1/framerate: kdur(2)-1/framerate;
+        kdata(kdata == 0) = NaN;
+        
+        for i = 1: size(kdata,3)
+           plot(timescale, kdata(:,:,i));
+           hold on;
+        end
+        title('Kymograph'), ylabel('Distance to soma [\mum]'), xlabel('Time [s]'),
+    end    
+    
+    %% DISPLAY STATISTICS %%
+    if dstats
+        density = density/iterations;
+        f4 = figure;
+        f4.Name = 'Statistics';
+        a = subplot(2,1,1);
+        scatter(a, eucl_tree(sneuron), density, 'r.')
+        title(a, 'Sholl Analysis'), ylabel(a, 'Number of particles'), xlabel(a, 'Distance from cell body [\mum]'),
+        b = subplot(2,1,2);
+        scatter(b, cvol_tree(sneuron), density, 'g.')
+        title(b, 'Volume'), ylabel(b, 'Number of particles'), xlabel(b, 'Segment volume [1/\mum]'),
+    end
+
+    if ~isempty(strfind(options, '-m'))
         close(v);
     end
+    
 end
